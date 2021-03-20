f3affd98601cc5ad4991825810d9cd51ab340a3e
Kirby Fracas <kafracas@gmail.com>
2021-03-20 09:47:58 -0500

    successfully migrated to mySQL

--- HappyBrainDump/Migrations/20210320144258_InitialMigration.Designer.cs
+++ HappyBrainDump/Migrations/20210320144258_InitialMigration.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using HappyBrainDump.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace HappyBrainDump.Data.Migrations
+namespace HappyBrainDump.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210320144258_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
